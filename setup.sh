35fcb7e293d91fc9fde2f0df75cb01ce70608847
Bob0824Bob <1977970249@qq.com>
2025-04-06 00:07:55 +1100

    Using tsinghua mirror

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # Download and install Miniconda
 MINICONDA_INSTALLER=Miniconda3-latest-Linux-x86_64.sh
-wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/$MINICONDA_INSTALLER -O ~/miniconda.sh
+wget https://repo.anaconda.com/miniconda/$MINICONDA_INSTALLER -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 
@@ -11,19 +11,20 @@ conda init
 source ~/.bashrc
 
 
-# Modify Conda to use Tsinghua mirror
-echo "channels:
-  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
-  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
-  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/r/
-  - defaults
-show_channel_urls: true" > ~/.condarc
+## Modify Conda to use Tsinghua mirror
+#echo "channels:
+#  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
+#  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
+#  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/r/
+#  - defaults
+#show_channel_urls: true" > ~/.condarc
 
 conda create -n gaussian_splatting python=3.12 -y
 conda activate gaussian_splatting
 
 # Install PyTorch and related dependencies
-pip3 install torch torchvision torchaudio plyfile -i https://pypi.tuna.tsinghua.edu.cn/simple
+#pip3 install torch torchvision torchaudio plyfile -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip3 install torch torchvision torchaudio plyfile
 
 # Install CUDA Toolkit and other required libraries
 conda install -y cudatoolkit tqdm
